0925bbda6a3b3896b875a7248452986ab7cfed89
Djay-M <dhananjay.m1805@gmail.com>
2023-04-14 17:23:27 +0530

    removed unwanted files, added code for multiple backgrounds

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
+const ConfigConstants = require("./config/constants");
+
+const { backgroundImage, backgroundColor } =
+  ConfigConstants.themesConfig["dark"];
+
+const style = {
+  backgroundColor: backgroundColor,
+  backgroundImage: `url(${backgroundImage})`,
+  minHeight: "100vh",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+};
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App" style={style}></div>;
 }
 
 export default App;
---
+++ src/config/constants.js
@@ -0,0 +1,13 @@
+import darkBackGround from "../assets/images/backgroundImages/darkBackGround.jpg";
+import lightBackGround from "../assets/images/backgroundImages/lightBackGround.jpg";
+
+export const themesConfig = {
+  dark: {
+    backgroundImage: darkBackGround,
+    backgroundColor: `#282c34`,
+  },
+  light: {
+    backgroundImage: lightBackGround,
+    backgroundColor: `#282c34`,
+  },
+};
